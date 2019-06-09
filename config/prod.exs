034f430fc64b962812f8db9f5d96bba224fe7cf1
Rafal Podlipny <rafal0p@outlook.com>
2019-06-09 10:24:51 +0200

    remove static content config from prod

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :pohmeedor, PohmeedorWeb.Endpoint,
   url: [scheme: "https", host: "salty-coast-20562", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
